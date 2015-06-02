c4eed88b2a2b9916c082bad64d7ae400aae35245
Pat Lathem <patl@codero.com>
2015-06-02 13:09:36 -0500

    Simplify executing ssh

--- server.go
+++ server.go
@@ -33,48 +33,32 @@ func (s Server) ssh() {
 
 	success := s.sshStartProcess()
 	for success == false {
-		handleWarning(fmt.Errorf("Unusual termination, reconnecting. Press Ctrl+C to abort."))
+		handleWarning(fmt.Errorf("Reconnecting. Press Ctrl+C to abort."))
 		time.Sleep(3000 * time.Millisecond)
 		success = s.sshStartProcess()
 	}
 }
 
 func (s Server) sshStartProcess() (success bool) {
-	/*
-		// TODO: use os.exec?
-		ssh, err := exec.LookPath("ssh")
-		handleError(err)
+	args := []string{"-p " + s.port, s.Hostname}
 
-		args := []string{"-p " + s.port, s.Hostname}
-		cmd := exec.Command(ssh, args...)
-		cmd.Start()
-		err = cmd.Wait()
-		handleError(err)
-		return err == nil
-	*/
-	cwd, err := os.Getwd()
+	cmd := exec.Command("ssh", args...)
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	cmd.Stdin = os.Stdin
+
+	err := cmd.Start()
 	handleError(err)
 
-	pa := os.ProcAttr{
-		Files: []*os.File{os.Stdin, os.Stdout, os.Stderr},
-		Dir:   cwd,
-	}
-
-	ssh, err := exec.LookPath("ssh")
-	handleError(err)
-
-	args := []string{ssh, "-p " + s.port, s.Hostname}
-	proc, err := os.StartProcess(ssh, args, &pa)
-	handleError(err)
-
-	state, err := proc.Wait()
+	err = cmd.Wait()
 	handleError(err)
 
 	// 127 is command not found
 	// 130 is ctrl+c
 	// TODO: is there not a way to get the return code as an int?
-	if state.String() == "exit status 127" || state.String() == "exit status 130" {
+	rs := cmd.ProcessState.String()
+	if rs == "exit status 127" || rs == "exit status 130" {
 		return true
 	}
-	return state.Success()
+	return cmd.ProcessState.Success()
 }
